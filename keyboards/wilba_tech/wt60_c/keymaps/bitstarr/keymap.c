64a1975ed9cc23249caee051189b1cdf093cd4c0
Sebastian Laube <git@laube.it>
2021-11-30 21:41:20 +0100

    added wt60 and tf60

---
+++ keyboards/wilba_tech/wt60_c/keymaps/bitstarr/keymap.c
@@ -0,0 +1,57 @@
+// Default layout for WT60-C
+#include QMK_KEYBOARD_H
+
+#define WORD_BACK C(KC_LEFT)
+#define WORD_FORWARD C(KC_RIGHT)
+
+enum layers {
+	_BASE,
+	_ADV,
+	_FN,
+	_UNI,
+	_UNI2,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_BASE] = LAYOUT_all(
+	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+	MO(_ADV), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(_FN),
+	KC_LCTL, KC_LGUI, KC_LALT,    LT(_UNI,KC_SPC),    MO(_ADV), MO(_UNI), KC_RCTL
+),
+
+[_ADV] = LAYOUT_all(
+	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
+	KC_TRNS, WORD_BACK, KC_UP, WORD_FORWARD, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_PSCR, KC_HOME, KC_END, KC_NO,
+	KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_PGDN, KC_TRNS,
+	KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_MUTE, KC_VOLU, KC_NO, KC_NO, KC_INS, KC_DEL, KC_TRNS, KC_NO,
+	KC_TRNS, KC_NO, KC_TRNS,    KC_TRNS,    KC_NO, KC_NO, KC_NO
+),
+
+[_FN] = LAYOUT_all(
+	KC_NO, H1_DEC, H1_INC, S1_DEC, S1_INC, H2_DEC, H2_INC, S2_DEC, S2_INC, EF_DEC, EF_INC, BR_DEC, BR_INC, KC_NO, KC_NO,
+	KC_NO, KC_NO, KC_NO, KC_NO, RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_END, KC_NO,
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_PGDN, KC_NO,
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+	KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO
+),
+
+[_UNI] = LAYOUT_all(
+	KC_NO, KC_NO, KC_NO, KC_NO, UC(L'§'), KC_NO, UC(L'°'), KC_NO, KC_NO, KC_NO, KC_NO, UC(L'–'), KC_NO, KC_NO, KC_NO,
+	KC_NO, KC_NO, KC_NO, UC(L'€'), KC_NO, KC_NO, KC_NO, UC(L'ü'), UC(L'ï'), UC(L'ö'), UC(L'„'), UC(L'“'), UC(L'”'), KC_NO,
+	MO(_UNI2), UC(L'ä'), UC(L'ß'), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UC(L'’'), KC_NO,
+	MO(_UNI2), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UC(L'…'), KC_NO, MO(_UNI2), KC_NO,
+	KC_NO, KC_NO, KC_NO,    KC_TRNS,    KC_NO, KC_NO, UC_MOD
+),
+
+[_UNI2] = LAYOUT_all(
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, UC(L'—'), KC_NO, KC_NO, KC_NO,
+	KC_NO, KC_NO, KC_NO, UC(L'é'), KC_NO, KC_NO, KC_NO, UC(L'Ü'), KC_NO, UC(L'Ö'), KC_NO, KC_NO, KC_NO, KC_NO,
+	KC_NO, UC(L'Ä'), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+	KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO
+),
+
+};
